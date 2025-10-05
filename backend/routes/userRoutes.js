772b83f4e990c6d6e8a3322617466d4401c827d1
sai krishna <saikrishnamamidi2003@gmail.com>
2025-10-05 21:43:42 +0530

    initial commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,75 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+
+const router = express.Router();
+//generating jwt token
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: "30",
+    });
+};
+
+
+// @desc    Register new user
+// @route   POST /api/users/register
+// @access  Public
+
+router.post("/register", asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body;
+
+    const userExists = await User.findOne({email});
+    if(userExists){
+        res.status(400);
+        throw new Error("User allready exists");
+    }
+
+    const user = await User.create({name , email, password});
+
+    if(user){
+        res.status(201).json({
+            _id : user.id,
+            name : user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        });
+    }
+    else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
+}
+));
+
+// @desc    Login user
+// @route   POST /api/users/login
+// @access  Public
+
+router.post(
+    "/login",
+    asyncHandler(async (req, res)=> {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+
+        if(user && (await user.matchPassword(password))){
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user._id),
+
+            });
+        }
+        else {
+            res.status(401);
+            throw new Error("Invalid email or password");
+        }
+    })
+);
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js"
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+//middleware
+
+app.use(express.json());
+app.use((cors()));
+
+//Routes
+app.use("/api/users", userRoutes);
+
+//test route
+app.get("/", (req, res)=>{
+    res.send("api is running..");
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> {
+    console.log(`server running on port ->  ${PORT}`);
+});
