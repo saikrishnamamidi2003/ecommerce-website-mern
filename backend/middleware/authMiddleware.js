04fd55f0bc2a4f9a055f0bb08a7a008dec720e80
sai krishna <saikrishnamamidi2003@gmail.com>
2025-10-12 23:03:59 +0530

    fixed some issues

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,23 +4,19 @@ import User from "../models/userModel.js";
 
 const protect = asyncHandler(async (req, res, next) => {
     let token;
-    if(
-        req.headers.authorization && 
-        req.headers.authorization.startsWith("Bearer ")
-    )
-    {
-        try{
-            token = req.headers.authorization.split(" ")[1];
-            const decoded = jwt.verify(token, process.env.jwt_SECRET);
-            req.user = await user.findById(decoded.id).select("-password");
-            next();
-        }
-        catch (error){
-            res.status(401);
-            throw new Error("Not authorized, token failed");
-        }
+const authHeader = req.headers['authorization'] || req.headers['Authorization']; 
+if (authHeader && authHeader.toString().trim().startsWith("Bearer ")) {
+    try {
+        token = authHeader.trim().split(" ")[1];
+        console.log("Token received after split:", token);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select("-password");
+        next();
+    } catch (error) {
+        res.status(401);
+        throw new Error("Not authorized, token failed");
     }
-
+}
     if(!token) {
         res.status(401);
         throw new Error("Not authorized, no token");
