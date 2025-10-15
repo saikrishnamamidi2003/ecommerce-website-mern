e61791a5ed43e676efb23ac842eda0fc1b076c83
sai krishna <saikrishnamamidi2003@gmail.com>
2025-10-15 14:36:02 +0530

    implemented the basic components and the routes

---
+++ client/src/pages/CartrPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CartPage() {
+  return <h2>Cart Page</h2>;
+}
+
+export default CartPage;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomePage(){
+    return <h2>Wellcome to the shopping website Home Page..</h2>
+}
+
+export default HomePage;
